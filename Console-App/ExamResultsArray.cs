be7868995cb741fa56eb101155070a6c5c24b16e
Srimathi.K <144440249+Its-Me-Srimathi@users.noreply.github.com>
2024-09-22 22:40:45 +0530

    Add files via upload

---
+++ Console-App/ExamResultsArray.cs
@@ -0,0 +1,42 @@
+﻿int Students;
+int Total;
+int NoOfSub = 6;
+Console.Write("\nEnter the number of Students : ");
+Students = Convert.ToInt32(Console.ReadLine());
+
+string[] Subject = new string[] { "English", "Tamil", "Maths", "Science", "Social", "Computer" };
+string[,] Records = new string[Students,NoOfSub + 4];
+
+for (int i= 0; i<Students; i++)
+{
+    Total = 0;
+    Console.Write("\nEnter the Student {0} Name : ", i+1);
+    Records[i, 0] = Console.ReadLine();
+    Records[i, 9] = "Pass";
+
+    for ( int j = 1; j<=NoOfSub; j++)
+    {
+        Console.Write($"Enter {Subject[j-1]} Marks : ");
+        Records[i,j] = Console.ReadLine();
+        Total += Convert.ToInt32(Records[i, j]);
+        if (Convert.ToInt32(Records[i, j]) < 40)
+        {
+            Records[i, 9] = "Fail";
+        }
+    }
+    Records[i, 8] = Total.ToString();
+}
+
+for( int i=0; i<Students; i++)
+{
+    Console.WriteLine("\nStudent Name : " + Records[i, 0]);
+     
+    for( int j = 1; j<=NoOfSub; j++)
+    {
+        Console.WriteLine(Subject[j-1] + ":" + Records[i,j]);
+    }
+    Console.WriteLine("Total : " + Records[i, 8]);
+    Console.WriteLine("Result : "+ Records[i, 9]);
+}
+
+
---
+++ Console-App/SalesJaggedArray.cs
@@ -0,0 +1,24 @@
+﻿int[][] WeeklySales = new int[4][];
+int OverallTotal = 0;
+
+WeeklySales[0] = new int[] { 300,550,240};
+WeeklySales[1] = new int[] { 800, 350, 600, 230, 500 };
+WeeklySales[2] = new int[] { 640, 400 };
+WeeklySales[3] = new int[] { 720, 400, 500, 100 };
+
+int[] TotalSalesPerWeek = new int[WeeklySales.Length];
+
+for(int i = 0; i<WeeklySales.Length; i++)    //total weekly sales
+{
+    for(int j = 0; j < WeeklySales[i].Length; j++ )          //each week total sales
+    {
+        TotalSalesPerWeek[i] += WeeklySales[i][j];
+    }
+    Console.WriteLine($"Total sales in week {i+1} : {TotalSalesPerWeek[i]}");
+}
+
+foreach(int WeekTotal in TotalSalesPerWeek)
+{
+    OverallTotal += WeekTotal;
+}
+Console.WriteLine("Overall Total Sales : " + OverallTotal);
